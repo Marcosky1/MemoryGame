fb00961d439a06f84ba26cd07a2820d1035f5c65
Marcosky1 <144145890+Marcosky1@users.noreply.github.com>
2024-06-29 15:53:10 -0500

    100%

--- Assets/Scripts/GameEndManager.cs
+++ Assets/Scripts/GameEndManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using System;
 
 public class GameEndManager : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class GameEndManager : MonoBehaviour
     public GameObject endGamePanel; // Panel que se muestra al final del juego
     public Text endGameTimeText;    // Texto en el panel final que muestra el tiempo
     public InputField playerNameInput; // Campo de entrada para el nombre del jugador
-    public Button submitButton;     // Botón para enviar los datos
+    public Button submitButton;     // BotÃ³n para enviar los datos
 
     private float timeElapsed;      // Tiempo transcurrido
     private bool gameRunning;       // Estado del juego (corriendo o no)
@@ -47,11 +48,12 @@ public class GameEndManager : MonoBehaviour
 
     private IEnumerator SendScoreToDatabase(string playerName, float time)
     {
-        WWWForm form = new WWWForm();
-        form.AddField("name", playerName);
-        form.AddField("time", time.ToString("F2"));
-
-        UnityWebRequest www = UnityWebRequest.Post("http://tuservidor.com/api/submit_score.php", form);
+        string jsonData = JsonUtility.ToJson(new ScoreData(playerName, time));
+        UnityWebRequest www = new UnityWebRequest("http://localhost/update_score_vj2.php", "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+        www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        www.SetRequestHeader("Content-Type", "application/json");
         yield return www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.Success)
@@ -64,9 +66,22 @@ public class GameEndManager : MonoBehaviour
             Debug.Log("Error submitting score: " + www.error);
         }
     }
+
     private void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    [System.Serializable]
+    private class ScoreData
+    {
+        public string username;
+        public string timeCompleted;
+
+        public ScoreData(string username, float time)
+        {
+            this.username = username;
+            this.timeCompleted = TimeSpan.FromSeconds(time).ToString(@"hh\:mm\:ss");
+        }
     }
 }
-
